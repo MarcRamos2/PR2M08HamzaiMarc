e736aad200a0e46af3e2789911f6d413fe8f29b4
Muhammad Hamza <mhamza.dam@institutcampalans.net>
2022-04-22 17:43:47 +0200

    setting
    e

---
+++ app/src/main/java/com/example/pr2_hamzaimarc/ui/login/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.pr2_hamzaimarc.ui.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pr2_hamzaimarc.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
